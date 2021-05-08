9f453f8ec0eb4ff97d19c31c32a2fd06ea27f649
Kevin Martin Jose <jskevin@amazon.com>
2021-05-08 17:45:37 +0200

    ir_measures untested

--- diffir/measure/qrels.py
+++ diffir/measure/qrels.py
@@ -1,4 +1,4 @@
-import pytrec_eval
+from ir_measures import iter_calc
 from profane import ModuleBase, Dependency, ConfigOption
 from diffir.measure import Measure
 
@@ -30,11 +30,25 @@ class QrelMeasure(Measure):
         qrels = dataset.qrels_dict()
         metric = self.config["metric"]
         topk = self.config["topk"]
-        evaluator = pytrec_eval.RelevanceEvaluator(qrels, {metric})
-        eval_run_1 = evaluator.evaluate(run1)
-        eval_run_2 = evaluator.evaluate(run2)
+        eval_run_1 = self.convert_to_nested_dict(iter_calc([metric], qrels, run1))
+        eval_run_2 = self.convert_to_nested_dict(iter_calc([metric], qrels, run2))
+
         query_ids = eval_run_1.keys() & eval_run_2.keys()
         query_ids = sorted(query_ids, key=lambda x: abs(eval_run_1[x][metric] - eval_run_2[x][metric]), reverse=True)
         query_ids = query_ids[:topk]
         id2diff = {x:abs(eval_run_1[x][metric] - eval_run_2[x][metric]) for x in query_ids}
+
         return query_ids, id2diff, metric
+
+    def convert_to_nested_dict(self, ir_measures_iterator):
+        """
+        Util method to convert the results from ir_measures.iter_calc to a dict.
+        TODO: We can probably refactor so that this method won't be needed
+        """
+        eval_dict = {}
+
+        for x in ir_measures_iterator:
+            # TODO: This assumes that there would be only one measure/metric to handle.
+            eval_dict[x.query_id] = {x.measure: x.value}
+
+        return eval_dict
