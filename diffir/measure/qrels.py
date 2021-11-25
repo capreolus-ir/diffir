0265d1120569100e2316e4a3f380368333a4db3a
Thong Nguyen <nguyenthacthong@gmail.com>
2021-11-26 00:25:12 +0100

    only pass qrels dict

--- diffir/measure/qrels.py
+++ diffir/measure/qrels.py
@@ -11,23 +11,20 @@ class QrelMeasure(Measure):
         :param run1: TREC run. Has the format {qid: {docid: score}, ...}
         :param run2: Same as above
         :param args:
-        :param kwargs: Expects a 'dataset' parameter. This is an instance of ir-datasets
+        :param kwargs: Expects a 'qrels' parameter containing qrels dict
         :return: A list of qids that differ the most in the metric
         """
-        assert "dataset" in kwargs, "Dataset object not supplied for qrel measure"
-        dataset = kwargs["dataset"]
-        assert dataset.has_qrels(), "Dataset object does not have the qrels files"
+        assert "qrels" in kwargs, "qrels is not supplied"
+        qrels = kwargs["qrels"]
         overlapping_keys = set(run1.keys()).intersection(set(run2.keys()))
         run1 = {qid: doc_id_to_score for qid, doc_id_to_score in run1.items() if qid in overlapping_keys}
         run2 = {qid: doc_id_to_score for qid, doc_id_to_score in run2.items() if qid in overlapping_keys}
-
-        qrels = dataset.qrels_dict()
         try:
             metric = parse_measure(self.metric)
         except NameError:
             print("Unknown measure: {}. Please provide a measure supported by https://ir-measur.es/".format(self.metric))
             sys.exit(1)
-
+            
         topk = self.topk
         eval_run_1 = self.convert_to_nested_dict(iter_calc([metric], qrels, run1))
         eval_run_2 = self.convert_to_nested_dict(iter_calc([metric], qrels, run2))
@@ -45,7 +42,6 @@ class QrelMeasure(Measure):
         TODO: We can probably refactor so that this method won't be needed
         """
         eval_dict = {}
-
         for x in ir_measures_iterator:
             # TODO: This assumes that there would be only one measure/metric to handle.
             eval_dict[x.query_id] = {x.measure: x.value}
