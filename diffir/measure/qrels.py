c7d15193df7b74c2164c8cbd7ff94175d3dc9c4a
Thong Nguyen <thongnguyenthac@gmail.com>
2022-11-06 17:40:05 +0100

    select queries with low measure

--- diffir/measure/qrels.py
+++ diffir/measure/qrels.py
@@ -6,6 +6,40 @@ from diffir.measure import Measure
 class QrelMeasure(Measure):
     module_name = "qrel"
 
+    def query_differences(self, run1, run2, *args, **kwargs):
+        """
+        :param run1: the first run
+        :param run2: the second run
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        if run1 and run2:
+            return self._query_differences(run1, run2, *args, **kwargs)
+        elif run1 and run2 is None:
+            return self._select_topk_single_run(run1, run2, *args, **kwargs)
+            # qids = sorted(list(run1.keys()))[: self.topk]
+            # id2diff = {qid: 0 for qid in qids}
+            # return qids, id2diff, "singlerun", None
+
+    def _select_topk_single_run(self, run1, run2, *args, **kwargs):
+        assert "qrels" in kwargs, "qrels is not supplied"
+        qrels = kwargs["qrels"]
+        query_ids = set(run1.keys())
+        try:
+            metric = parse_measure(self.metric)
+        except NameError:
+            print("Unknown measure: {}. Please provide a measure supported by https://ir-measur.es/".format(self.metric))
+            sys.exit(1)
+
+        topk = self.topk
+        eval_run_1 = self.convert_to_nested_dict(iter_calc([metric], qrels, run1))
+        query_ids = sorted(query_ids, key=lambda x: eval_run_1[x][metric])
+        query_ids = query_ids[:topk]
+        id2diff = {x: 0 for x in query_ids}
+        id2qrelscores = {x: [eval_run_1[x][metric], 0] for x in query_ids}
+        return query_ids, id2diff, self.metric, id2qrelscores
+
     def _query_differences(self, run1, run2, *args, **kwargs):
         """
         :param run1: TREC run. Has the format {qid: {docid: score}, ...}
@@ -24,7 +58,7 @@ class QrelMeasure(Measure):
         except NameError:
             print("Unknown measure: {}. Please provide a measure supported by https://ir-measur.es/".format(self.metric))
             sys.exit(1)
-            
+
         topk = self.topk
         eval_run_1 = self.convert_to_nested_dict(iter_calc([metric], qrels, run1))
         eval_run_2 = self.convert_to_nested_dict(iter_calc([metric], qrels, run2))
